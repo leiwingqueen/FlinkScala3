a99086e9fe146e44a2a40df8f2223d0af79d3984
leiwingqueen <leiwingqueen@gmail.com>
2025-08-03 10:13:13 +0800

    修复编译问题

--- src/main/scala/modernflink/section1/MultipleStreams.scala
+++ src/main/scala/modernflink/section1/MultipleStreams.scala
@@ -4,7 +4,7 @@ import org.apache.flinkx.api.serializers.*
 import org.apache.flinkx.api.{ConnectedStreams, DataStream, KeyedStream, StreamExecutionEnvironment}
 import modernflink.model.{HumidityReading, LocalSummary, TemperatureReading}
 
-val env = StreamExecutionEnvironment.getExecutionEnvironment
+// val env = StreamExecutionEnvironment.getExecutionEnvironment
 
 val humidityDataStream: KeyedStream[HumidityReading, String] = env
   .readTextFile("src/main/resources/Humidity.txt")
